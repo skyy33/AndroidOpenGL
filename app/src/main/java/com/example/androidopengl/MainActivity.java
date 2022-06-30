e0f77413b719d8ccf33ceff466d2dbf26b2d9d14
李盼 <beyond.me@qq.com>
2022-06-30 15:11:19 +0800

    提交

--- app/src/main/java/com/example/androidopengl/MainActivity.java
+++ app/src/main/java/com/example/androidopengl/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.androidopengl;
 
-import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.app.Activity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends Activity{
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +17,7 @@ public class MainActivity extends AppCompatActivity {
 
         OneGlSurfaceView oneGlSurfaceView =new OneGlSurfaceView(this);
         setContentView(oneGlSurfaceView);
+
     }
+
 }
